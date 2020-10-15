8f77de2093d1929aa674cefcd30b1a7b4de7bc87
ORiveraJr84 <oriverajr84@gmail.com>
2020-10-15 05:16:41 -0400

    Wrapped nav in a header and routed links to pages.

--- subreddit_posthere/src/Components/Nav.js
+++ subreddit_posthere/src/Components/Nav.js
@@ -1,40 +1,59 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 import styled, { css } from "styled-components";
 
-const NavWrapper = styled.div`
-  background-color: ${(props) => props.theme.mutedAccentColor};
+const Header = styled.div`
+  align-items: center;
+  background-color: ${(props) => props.theme.accentColor};
   display: flex;
-  justify-content: flex-end;
-  align-items: baseline;
-  max-width: 100vw;
-  padding: 1rem;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem;
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  h1 {
+    font-family: ${(props) => props.theme.headerFont};
+    font-size: 3rem;
+  }
 `;
 
-const LinkWrapper = styled.div`
-  width: 25%;
+const NavWrapper = styled.div`
+  display: flex;
+  height: 12vh;
+  align-items: baseline;
+`;
+
+const LinksWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
   margin-right: 1rem;
+  text-align: center;
 `;
 
 const StyledNavLink = styled(NavLink)`
-  /* padding: 0.5rem 0px; */
+  padding: 0.5rem;
   box-sizing: border-box;
+  font-family: ${(props) => props.theme.bodyFont};
+  box-sizing: border-box;
+  white-space: nowrap;
 
   &:visited {
     color: inherit;
   }
 
-  &:hover {
+  &:hover,
+  &:focus {
     color: ${(props) => props.theme.highlightColor};
     border-bottom: 5px solid ${(props) => props.theme.primaryColor};
+    font-weight: 600;
   }
 `;
 
 const ButtonWrapper = styled.div`
-  width: 12%;
+  /* width: 12.5%; */
   display: flex;
   justify-content: space-between;
 `;
@@ -45,6 +64,7 @@ const Button = styled.button`
   padding: 0.5rem 1rem;
   font-size: 1rem;
   cursor: pointer;
+  font-family: ${(props) => props.theme.bodyFont};
 
   &:hover {
     background-color: ${(props) => props.theme.midtoneColor};
@@ -58,27 +78,44 @@ const LogInButton = styled(Button)`
 `;
 
 const Nav = () => {
+  let history = useHistory();
+
+  const logIn = () => {
+    history.push("/login");
+  };
+
+  const signUp = () => {
+    history.push("/signup");
+  };
+
   return (
-    <NavWrapper>
-      <LinkWrapper>
-        <StyledNavLink exact to="/">
-          Home
-        </StyledNavLink>
-        <StyledNavLink exact to="/aboutUs">
-          About Us
-        </StyledNavLink>
-        <StyledNavLink exact to="/somethingElse">
-          Something Else
-        </StyledNavLink>
-        <StyledNavLink exact to="/someOtherThing">
-          Some Other Thing
-        </StyledNavLink>
-      </LinkWrapper>
-      <ButtonWrapper>
-        <SignUpButton id="signUpButton">Sign Up</SignUpButton>
-        <LogInButton id="logInButton">Log In</LogInButton>
-      </ButtonWrapper>
-    </NavWrapper>
+    <Header>
+      <h1>SubReddit</h1>
+      <NavWrapper>
+        <LinksWrapper>
+          <StyledNavLink exact to="/">
+            Home
+          </StyledNavLink>
+          <StyledNavLink exact to="/aboutUs">
+            About Us
+          </StyledNavLink>
+          <StyledNavLink exact to="/somethingElse">
+            Something Else
+          </StyledNavLink>
+          <StyledNavLink exact to="/someOtherThing">
+            Some Other Thing
+          </StyledNavLink>
+        </LinksWrapper>
+        <ButtonWrapper>
+          <SignUpButton id="signUpButton" onClick={signUp}>
+            Sign Up
+          </SignUpButton>
+          <LogInButton id="logInButton" onClick={logIn}>
+            Log In
+          </LogInButton>
+        </ButtonWrapper>
+      </NavWrapper>
+    </Header>
   );
 };
 
