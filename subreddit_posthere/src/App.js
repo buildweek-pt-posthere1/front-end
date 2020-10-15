0ae74663a6ebc70716e8da78f4f965c69ec1e69c
ORiveraJr84 <oriverajr84@gmail.com>
2020-10-14 23:28:24 -0400

    Started working on routing for pages in App.js

--- subreddit_posthere/src/App.js
+++ subreddit_posthere/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Dashboard from "./Components/Dashboard";
 import Nav from "./Components/Nav";
+import "./reset.css";
 import "./App.css";
+import { Route, Switch } from "react-router";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Nav />
-        <h1>Subreddit Predictor</h1>
-      </header>
+      <Nav />
+      <Switch>
+        <Route path="/login">This should go to the Log In Page</Route>
+        <Route path="/signup">This should go to the Sign Up Page</Route>
+        <Route path="/dashboard">This should go to the Dashboard Page</Route>
+        <Route path="/">This should go to the Marketing AKA Home Page</Route>
+      </Switch>
     </div>
   );
 }
