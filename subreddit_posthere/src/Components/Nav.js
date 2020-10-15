5c9cfa792e4d4d303709bd3e4249be49a1faa39b
ORiveraJr84 <oriverajr84@gmail.com>
2020-10-15 02:51:53 -0400

    Working the Nav and styling as needed.

--- subreddit_posthere/src/Components/Nav.js
+++ subreddit_posthere/src/Components/Nav.js
@@ -1,24 +1,83 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import styled from "styled-components";
-import colorScheme from "../colorScheme";
+import styled, { css } from "styled-components";
 
 const NavWrapper = styled.div`
-  background-color: ${colorScheme.accentColor};
+  background-color: ${(props) => props.theme.mutedAccentColor};
   display: flex;
   justify-content: flex-end;
+  align-items: baseline;
   max-width: 100vw;
   padding: 1rem;
+`;
+
+const LinkWrapper = styled.div`
+  width: 25%;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  margin-right: 1rem;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  /* padding: 0.5rem 0px; */
+  box-sizing: border-box;
+
+  &:visited {
+    color: inherit;
+  }
+
+  &:hover {
+    color: ${(props) => props.theme.highlightColor};
+    border-bottom: 5px solid ${(props) => props.theme.primaryColor};
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  width: 12%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => props.theme.highlightColor};
+  border-radius: ${(props) => props.theme.radius};
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.theme.midtoneColor};
+  }
+`;
+
+const SignUpButton = styled(Button)``;
+
+const LogInButton = styled(Button)`
+  background-color: ${(props) => props.theme.primaryColor};
 `;
 
 const Nav = () => {
   return (
     <NavWrapper>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/aboutUs">About Us</NavLink>
-      <NavLink to="/somethingElse">Something Else</NavLink>
-      <button>Sign Up</button>
-      <button>Log In</button>
+      <LinkWrapper>
+        <StyledNavLink exact to="/">
+          Home
+        </StyledNavLink>
+        <StyledNavLink exact to="/aboutUs">
+          About Us
+        </StyledNavLink>
+        <StyledNavLink exact to="/somethingElse">
+          Something Else
+        </StyledNavLink>
+        <StyledNavLink exact to="/someOtherThing">
+          Some Other Thing
+        </StyledNavLink>
+      </LinkWrapper>
+      <ButtonWrapper>
+        <SignUpButton id="signUpButton">Sign Up</SignUpButton>
+        <LogInButton id="logInButton">Log In</LogInButton>
+      </ButtonWrapper>
     </NavWrapper>
   );
 };
