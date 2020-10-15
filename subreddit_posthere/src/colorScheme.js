8336acaf74d02da3cfad37b0a2ab03c68822f864
ORiveraJr84 <oriverajr84@gmail.com>
2020-10-14 23:28:52 -0400

    Added and created colorScheme file to keep the look consistent.

---
+++ subreddit_posthere/src/colorScheme.js
@@ -0,0 +1,9 @@
+const colorScheme = {
+  primaryColor: "#5296dd",
+  secondaryColor: "#92bddf",
+  midtoneColor: "#d7d7d7",
+  accentColor: "#ff6314",
+  highlightColor: "ffffff",
+};
+
+export default colorScheme;
