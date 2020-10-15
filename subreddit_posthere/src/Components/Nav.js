ec099561966bc67b9367c36363b463315ad10b79
ORiveraJr84 <oriverajr84@gmail.com>
2020-10-14 23:30:26 -0400

    Started working on the Nav.js file.

--- subreddit_posthere/src/Components/Nav.js
+++ subreddit_posthere/src/Components/Nav.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import colorScheme from "../colorScheme";
 
-const NavWrapper = styled.div``;
+const NavWrapper = styled.div`
+  background-color: ${colorScheme.accentColor};
+  display: flex;
+  justify-content: flex-end;
+  max-width: 100vw;
+  padding: 1rem;
+`;
 
 const Nav = () => {
   return (
     <NavWrapper>
-      <Link>Navigation Bar</Link>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/aboutUs">About Us</NavLink>
+      <NavLink to="/somethingElse">Something Else</NavLink>
+      <button>Sign Up</button>
+      <button>Log In</button>
     </NavWrapper>
   );
 };
