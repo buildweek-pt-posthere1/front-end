ca4f0f5fc3fd9178fb22be7c3966abd3a0734b8e
ORiveraJr84 <oriverajr84@gmail.com>
2020-10-15 02:51:26 -0400

    Added theme to include other things besides color.

---
+++ subreddit_posthere/src/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  primaryColor: "#5296dd",
+  secondaryColor: "#92bddf",
+  midtoneColor: "#d7d7d7",
+  accentColor: "#ff6314",
+  mutedAccentColor: "#FF8547",
+  highlightColor: "#ffffff",
+  radius: "5px",
+};
+
+export default theme;
