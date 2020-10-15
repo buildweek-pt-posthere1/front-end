a17c598e2fb316316cd24176b3f9e21c17931771
ORiveraJr84 <oriverajr84@gmail.com>
2020-10-15 02:50:36 -0400

    Worked with theme a bit more.

--- subreddit_posthere/src/App.js
+++ subreddit_posthere/src/App.js
@@ -6,18 +6,24 @@ import Nav from "./Components/Nav";
 import "./reset.css";
 import "./App.css";
 import { Route, Switch } from "react-router";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
+
+console.log("theme", theme);
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Switch>
-        <Route path="/login">This should go to the Log In Page</Route>
-        <Route path="/signup">This should go to the Sign Up Page</Route>
-        <Route path="/dashboard">This should go to the Dashboard Page</Route>
-        <Route path="/">This should go to the Marketing AKA Home Page</Route>
-      </Switch>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/login">This should go to the Log In Page</Route>
+          <Route path="/signup">This should go to the Sign Up Page</Route>
+          <Route path="/dashboard">This should go to the Dashboard Page</Route>
+          <Route path="/">This should go to the Marketing AKA Home Page</Route>
+        </Switch>
+      </div>
+    </ThemeProvider>
   );
 }
 
