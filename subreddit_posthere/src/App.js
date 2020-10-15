df3b9921fdd8e944359d9d993eee9b15f6b17f7b
unknown <erickvillegasecheverria@gmail.com>
2020-10-14 20:30:24 -0700

    Unfinished Login Page

--- subreddit_posthere/src/App.js
+++ subreddit_posthere/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "./Components/Nav";
 import "./App.css";
+import Login from './Components/Login'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Nav />
         <h1>Subreddit Predictor</h1>
       </header>
+      <Login/>
     </div>
   );
 }
---
+++ subreddit_posthere/src/Components/Login.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import { FormControl, FormGroup, TextField,Button,Box } from "@material-ui/core";
+import * as yup from 'yup';
+import { findByLabelText } from "@testing-library/react";
+import axios from 'axios';
+
+
+export default function LogIn() {
+    
+    // Styling
+    const container_style = {
+        display:"flex",
+        flexDirection:'row',
+        height:'90vh',
+        
+    }
+    const smallcontainer_style={
+        width:'50%',
+        display:'flex',
+        alignItems:'center',
+        
+        flexDirection:'column',
+    }
+    const img_style = {
+        height:'100%'
+    }
+    // Styling ends
+
+    const [login, setLogin] = useState({
+    email: "",
+    password: ""
+    });
+
+    const [disable,setDisable] = useState(true)
+
+    const formSchema = yup.object().shape({
+        username: yup.string().required("Username is a required field."), 
+        password: yup.string().required("Password is a required field.")
+    })
+
+    useEffect(() => {
+            formSchema.isValid(login).then(valid => {
+            console.log('valid?', valid)
+            setDisable(!valid);
+        });
+    }, [login]);
+
+    const handleChange = (event) => {
+    const newValue = { ...login, [event.target.name]: event.target.value };
+    setLogin(newValue);
+    };
+
+    const onSubmit =(event) => {
+        event.preventDefault();
+        axios
+            .get('')
+            .then(response => {
+                
+            })         
+    }
+
+    return (
+        <div style={container_style}>
+            <div style={smallcontainer_style}>
+                <img src='https://smirknewmedia.com/wp-content/uploads/2018/03/snoo.jpg' style={img_style}/>
+            </div>
+            <div style={smallcontainer_style }> 
+            <FormControl style={{paddingTop:'33%'}}>
+                <FormGroup style={{ margin: "5px" }}>
+                    <TextField
+                    id="username"
+                    name="username"
+                    label="Username"
+                    variant="outlined"
+                    value={login.username}
+                    onChange={handleChange}
+                    style={{color:'white'}}
+                    />
+                </FormGroup>
+                <FormGroup style={{ margin: "5px" }}>
+                <TextField
+                    type="password"
+                    id="password"
+                    name="password"
+                    label="Password"
+                    variant="outlined"
+                    value={login.password}
+                    onChange={handleChange}
+                    style={{color:'white'}}
+                />
+                </FormGroup>
+                <Button disabled={disable} type='submit' variant="contained" color="primary">Let's go!</Button>
+            </FormControl>
+            <Box>Not a user?</Box>
+            <Button variant="contained" color="primary">Signup!</Button>
+            </div>
+        </div>
+    );
+}
