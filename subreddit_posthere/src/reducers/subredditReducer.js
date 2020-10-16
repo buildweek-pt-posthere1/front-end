378857b51d3c0c8e3e6fb64edeb55f2eba8275c7
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-10-16 00:11:01 -0400

    setup thunk and subredditreducer

---
+++ subreddit_posthere/src/reducers/subredditReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  signUp: {
+    username: "",
+    password: "",
+  },
+};
+
+export const subredditReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
